f1e558e0a2eee9696c342ed6612d590b50f278b1
Majid alDosari <majidaldosari-github@yahoo.com>
2020-08-16 16:16:14 -0400

    [project] further isolate project tasks

---
+++ project/tasks.py
@@ -0,0 +1,7 @@
+from estcp_project.tasks import ns
+
+from invoke import Collection
+
+
+#ns.add_collection(Collection.from_module(estcp_project.tasks), name=None)
+#ns.add_collection(Collection.from_module(docs))

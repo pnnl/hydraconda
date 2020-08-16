d155902f9fa75c62dc2617f1aa3873b5554f59db
Majid alDosari <majidaldosari-github@yahoo.com>
2020-08-15 20:19:58 -0400

    [project][notebooking] initial notebooking commit

---
+++ notebooking/book/Untitled.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.5.2
+#   kernelspec:
+#     display_name: Python [conda env:estcp-notebooking] *
+#     language: python
+#     name: conda-env-estcp-notebooking-py
+# ---
+
+# %% [markdown]
+# ### test
+# you should specify a kernel associated with the project
+
+# %%
+1+1
---
+++ notebooking/readme.md
@@ -0,0 +1,5 @@
+experimental centralization of notebooking
+
+* activate jupyter server through the wrapper from (not going into the conda environment)
+* have to have ipykernel installed in the devenv
+* use jupytext to pair notebook with a text file: File->jupytext. That will get into source control. Notebooks will not be saved to source. Enforced with [jupytext.yml](../.jupytext.yaml).

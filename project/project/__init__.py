58fece14c5b5791e15f5183fa12697ae8757ed7a
Majid alDosari <majidaldosari-github@yahoo.com>
2020-11-03 15:15:33 -0500

    [project] add activated work dir var

--- project/project/__init__.py
+++ project/project/__init__.py
@@ -2,7 +2,13 @@ from pyprojroot import here
 from pathlib import Path
 import os
 project_root_dir = project_root = root_dir = here(project_files=('.here', 'bootstrap.sh', 'bootstrap.bat'))
+work_dir = os.environ.get('WORKDIR')
+assert(work_dir)
+activated_work_dir = activated_workdir = project_root / work_dir
 project_name = os.environ.get('PROJECT_NAME')
 assert(project_name)
 del here, Path, os
-__all__ = ['root_dir', 'project_root', 'project_root_dir', 'project_name']
+__all__ = [
+    'root_dir', 'project_root', 'project_root_dir',
+    'project_name' ,
+    'activated_work_dir', 'activated_workdir',]

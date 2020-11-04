b74995d9c8c4d488c666bace2bdbe4fb1f341ee5
Majid alDosari <majidaldosari-github@yahoo.com>
2020-11-04 10:24:29 -0500

    [project] try to get project root from env var in case here messes up

--- project/project/__init__.py
+++ project/project/__init__.py
@@ -1,13 +1,18 @@
-from pyprojroot import here
 from pathlib import Path
 import os
-project_root_dir = project_root = root_dir = here(project_files=('.here', 'bootstrap.sh', 'bootstrap.bat'))
+try:
+    from pyprojroot import here
+    project_root_dir = project_root = root_dir = here(project_files=('.here', 'bootstrap.sh', 'bootstrap.bat'))
+    del here
+except:
+    project_root_dir = project_root = root_dir = Path(os.environ.get('PROJECT_ROOT'))
+    
 work_dir = os.environ.get('WORKDIR')
 assert(work_dir)
 activated_work_dir = activated_workdir = project_root / work_dir
 project_name = os.environ.get('PROJECT_NAME')
 assert(project_name)
-del here, Path, os
+del Path, os, work_dir
 __all__ = [
     'root_dir', 'project_root', 'project_root_dir',
     'project_name' ,

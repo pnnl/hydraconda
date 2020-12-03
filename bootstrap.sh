67b9c92c47079013058266f24de5d89150e7e6d5
Majid alDosari <majidaldosari-github@yahoo.com>
2020-12-03 15:07:03 -0500

    [project] fix: no spaces to assign var in bash

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-PROJECT_NAME = $(python project_name.py)
-PROJECT_ENV = ${PROJECT_NAME}-project
+PROJECT_NAME=$(python project_name.py)
+PROJECT_ENV=${PROJECT_NAME}-project
 
 conda env update --name=base
 cd project

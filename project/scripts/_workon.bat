7d9e3d7b20ada6d6a1bd872504c5bd93b0525870
Majid alDosari <majidaldosari-github@yahoo.com>
2020-12-22 00:08:51 -0500

    [project] fixing experimental

--- project/scripts/_workon.bat
+++ project/scripts/_workon.bat
@@ -2,15 +2,14 @@ echo >/dev/null # >nul & GOTO WINDOWS & rem ^
 set -e
 first_arg="$1"
 shift
-cd ${PROJECT_ROOT}/first_arg
-project work-on -w first_arg "$@"
-activate first_arg
+cd ${PROJECT_ROOT}/${first_arg}
+project work-on -w ${first_arg} "$@"
+activate ${first_arg}
 exit 0
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-REM experimental dont use
 :WINDOWS
 set _tail=%*
 call set _tail=%%_tail:*%1=%%
 cd %PROJECT_ROOT%\%1
 project work-on -w %1 %_tail%
-activate %1
+conda activate %1
--- project/scripts/_workon.sh
+++ project/scripts/_workon.sh
@@ -1,11 +1,10 @@
 echo >/dev/null # >nul & GOTO WINDOWS & rem ^
-echo experimental dont use
 set -e
 first_arg="$1"
 shift
-cd ${PROJECT_ROOT}/first_arg
-project work-on -w first_arg "$@"
-conda activate first_arg
+cd ${PROJECT_ROOT}/${first_arg}
+project work-on -w ${first_arg} "$@"
+activate ${first_arg}
 exit 0
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 :WINDOWS

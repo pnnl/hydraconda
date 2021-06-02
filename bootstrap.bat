e3e67bc143135a862bb51c0d6b7fc6fa16c6cc5a
Majid alDosari <majidaldosari-github@yahoo.com>
2021-06-02 09:11:45 -0700

    fix: no space in goto label

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,18 +1,18 @@
 @echo on
 
 
-call            python project_name.py || goto :error
+call            python project_name.py || goto:error
 for /f %%i in ('python project_name.py') do set PROJECT_NAME=%%i
 set PROJECT_ENV=%PROJECT_NAME%-project
 
-call conda env update --name=base || goto :error
-call cd project || goto :error
-call conda devenv || goto :error
-call conda run -n %PROJECT_ENV% --live-stream invoke work-dir.action.create-scripts-wrappers || goto :error
-call copy /Y wbin\project-run-in.bat wbin\_project-run-in.bat || goto :error
-call wbin\_project-run-in invoke work-on --work-dir=project || goto :error
-call cd .. || goto :error
-goto :EOF
+call conda env update --name=base || goto:error
+call cd project || goto:error
+call conda devenv || goto:error
+call conda run -n %PROJECT_ENV% --live-stream invoke work-dir.action.create-scripts-wrappers || goto:error
+call copy /Y wbin\project-run-in.bat wbin\_project-run-in.bat || goto:error
+call wbin\_project-run-in invoke work-on --work-dir=project || goto:error
+call cd .. || goto:error
+goto:EOF
 
 :error
 exit /b %errorlevel%"
